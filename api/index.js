806a68bed2b9055f5816f10337044891f79e94e2
shreyasibhagwat6 <shreyasibhagwat6@gmail.com>
2024-08-12 22:54:40 -0700

    added data validation to sign up form

--- api/index.js
+++ api/index.js
@@ -10,24 +10,39 @@ app.use(express.json());
 
 const prisma = new PrismaClient();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', async (req, res) => {
-  const allUsers = await prisma.user.findMany();
-  res.json(allUsers);
+// const users = [];
+
+// app.get('/', (req, res) => {
+//   res.json(users);
+// })
+
+app.post('/auth/signup', (req, res) => {
+  console.log(req.body);
+  const newUser = { name: req.body.name, email: req.body.email, username: req.body.username, password: req.body.password }
+  res.send(newUser);
 })
 
-app.post("/", async (req, res) => {
-  const { firstname, lastname } = req.body
-    const newUser = await prisma.user.create({ 
-      data: { 
-      firstname, 
-      lastname,
-      } 
-    });
-    const users = await prisma.user.findMany();
-    res.json(users);
-})
+// app.get('/', async (req, res) => {
+//   const allUsers = await prisma.user.findMany();
+//   res.json(allUsers);
+// })
+
+// app.post("/", async (req, res) => {
+//   const { name, email, username, password } = req.body
+//     const newUser = await prisma.user.create({ 
+//       data: { 
+//         name,
+//         email,
+//         username,
+//         password
+//       } 
+//     });
+//     const users = await prisma.user.findMany();
+//     res.json(users);
+// })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
